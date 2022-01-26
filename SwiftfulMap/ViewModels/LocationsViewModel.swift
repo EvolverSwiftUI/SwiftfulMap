bbff6c8be831f2f2bd3a14a906699c105ef6dc04
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-01-26 20:38:25 +0530

    Added map to view

--- SwiftfulMap/ViewModels/LocationsViewModel.swift
+++ SwiftfulMap/ViewModels/LocationsViewModel.swift
@@ -5,14 +5,39 @@
 //  Created by Sivaram Yadav on 1/26/22.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on Map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion
+                = MKCoordinateRegion(
+                    center: location.coordinates,
+                    span: mapSpan
+                )
+        }
     }
 }
--- SwiftfulMap/Views/LocationsView.swift
+++ SwiftfulMap/Views/LocationsView.swift
@@ -6,17 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
 //    @StateObject private var vm = LocationsViewModel()
     @EnvironmentObject private var vm: LocationsViewModel
     
+    // This mapRegion will need to move ViewModel
+    //@State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
+    //    center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+    //    span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    //)
+    
     var body: some View {
-        List {
-            ForEach(vm.locations) { location in
-                Text(location.name)
-            }
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
         }
     }
 }
