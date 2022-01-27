276bc99ee2a56e954c34ab1dfe804af486adf954
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-01-27 23:51:17 +0530

    Added Location Preview Card and did some logic around to display UI and also added next button functionality

--- SwiftfulMap/ViewModels/LocationsViewModel.swift
+++ SwiftfulMap/ViewModels/LocationsViewModel.swift
@@ -55,4 +55,27 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
         }
     }
+    
+    func nextButtonTapped() {
+        
+        // Get the current index
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else {
+            debugPrint("Could not find out current location! Should never happen.")
+            return
+        }
+        
+        // Check if currentIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next is not valid
+            // Restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // nextIndex is valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
 }
---
+++ SwiftfulMap/Views/LocationPreviewView.swift
@@ -0,0 +1,107 @@
+//
+//  LocationPreviewView.swift
+//  SwiftfulMap
+//
+//  Created by Sivaram Yadav on 1/26/22.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    let location: Location
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(alignment: .leading, spacing: 16) {
+                imageSection
+                titleSection
+            }
+
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white.opacity(0.9))
+                .offset(y: 65)
+        )
+        //.clipped()
+        .cornerRadius(10)
+    }
+}
+
+struct LocationPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            LocationPreviewView(location: LocationsDataService.locations.first!)
+                .padding()
+        }
+        .environmentObject(LocationsViewModel())
+    }
+}
+
+extension LocationPreviewView {
+    
+    private var imageSection: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(Color.red.opacity(0.8))
+        .cornerRadius(10)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("Learn More")
+                .font(.headline)
+                .frame(width: 125, height: 40)
+                .foregroundColor(.white)
+                .background(Color.red)
+                .cornerRadius(8)
+        })
+    }
+    
+    private var nextButton: some View {
+        Button(action: {
+            vm.nextButtonTapped()
+        }, label: {
+            Text("Next")
+                .font(.headline)
+                .fontWeight(.bold)
+                .frame(width: 125, height: 35)
+                .foregroundColor(.red)
+                .background(Color.black.opacity(0.2))
+                .cornerRadius(8)
+        })
+    }
+
+}
